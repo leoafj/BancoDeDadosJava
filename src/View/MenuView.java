43f8f9550e896e45472050a2b46db1df5cbc58f2
Leonardo Freitas <leoafj@hotmail.com>
2022-02-10 16:37:47 -0300

    prevenindo do SQL inject

--- src/View/MenuView.java
+++ src/View/MenuView.java
@@ -30,7 +30,7 @@ public class MenuView extends javax.swing.JFrame {
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jMenu1.setText("File");
         jMenuBar1.add(jMenu1);
--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -26,17 +26,23 @@ public class UsuarioDAO {
     
     public void insert (Usuario usuario) throws SQLException{
         
-        String sql = "insert into usuario (usuario, senha) values ('"+ usuario.getUsuario() +"','"+ usuario.getSenha() +"'); ";
+        String sql = "insert into usuario (usuario, senha) values (?, ?); ";
             
             PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, usuario.getUsuario());
+            statement.setString(2, usuario.getSenha());
             statement.execute();
          
         }
 
     public boolean existeNoBancoPorUsuarioESenha(Usuario usuario) throws SQLException {
         
-        String sql = "select * from usuario where usuario = '"+ usuario.getUsuario() +"' and senha = '"+ usuario.getSenha() +"' ";
+        String sql = "select * from usuario where usuario = ? and senha = ?";
+        
         PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, usuario.getUsuario());
+        statement.setString(2, usuario.getSenha());
+        
         statement.execute();
             
         ResultSet resultSet = statement.getResultSet();
